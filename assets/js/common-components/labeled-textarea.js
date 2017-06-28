729e8550560c37b050085e680913c1c390804c55
Aleksandr Vishnyakov <av@virtoway.com>
2017-06-28 17:36:37 +0500

    Refactored invitation form

---
+++ assets/js/common-components/labeled-textarea.js
@@ -0,0 +1,24 @@
+﻿angular.module('storefrontApp')
+
+.component('vcLabeledTextArea', {
+    templateUrl: "themes/assets/labeled-textarea.tpl.html",
+    bindings: {
+        value: '=',
+        form: '=',
+        name: '@',
+        label: '@',
+        required: '<',
+        requiredError: '@?',
+        pattern: '<?',
+        autofocus: '<'
+    },
+    controller: [function () {
+        var $ctrl = this;
+
+        $ctrl.validate = function () {
+            $ctrl.form.$setSubmitted();
+            return $ctrl.form.$valid;
+        };
+
+    }]
+});
