9dc51030662d1d1f55ee4b8d2a9595047eda3958
Artyom Androsov <aan@virtoway.com>
2017-07-04 13:10:27 +0200

    Title field in profile page. Redirect actions from Profile and Addresses to the B2B Extensions module

---
+++ assets/js/account/account-addresses.js
@@ -0,0 +1,72 @@
+﻿angular.module('storefront.account')
+.component('vcAccountAddresses', {
+    templateUrl: "themes/assets/js/account/account-addresses.tpl.liquid",
+    require: {
+        accountManager: '^vcAccountManager'
+    },
+    controller: ['storefrontApp.mainContext', 'confirmService', '$translate', '$scope', 'loadingIndicatorService', 'storefront.corporateAccountApi', function (mainContext, confirmService, $translate, $scope, loader, corporateAccountApi) {
+        var $ctrl = this;
+        $ctrl.loader = loader;
+        $ctrl.currentMemberId = mainContext.customer.id;
+
+        this.$routerOnActivate = function (next) {
+            loader.wrapLoading(function () {
+                return corporateAccountApi.getCompanyMember({ id: $ctrl.currentMemberId }, function (member) {
+                    $ctrl.currentMember = member;
+                }).$promise;
+            });
+        };
+
+        $ctrl.addNewAddress = function () {
+            if (_.last(components).validate()) {
+                $ctrl.currentMember.addresses.push($ctrl.newAddress);
+                $ctrl.newAddress = null;
+                $ctrl.updateCompanyMember($ctrl.currentMember);
+            }
+        };
+
+        $ctrl.submit = function () {
+            if (components[$ctrl.editIdx].validate()) {
+                angular.copy($ctrl.editItem, $ctrl.currentMember.addresses[$ctrl.editIdx]);
+                $ctrl.updateCompanyMember($ctrl.currentMember).then($ctrl.cancel);
+            }
+        };
+
+        $ctrl.cancel = function () {
+            $ctrl.editIdx = -1;
+            $ctrl.editItem = null;
+        };
+
+        $ctrl.edit = function ($index) {
+            $ctrl.editIdx = $index;
+            $ctrl.editItem = angular.copy($ctrl.currentMember.addresses[$ctrl.editIdx]);
+        };
+
+        $ctrl.delete = function ($index) {
+            var showDialog = function (text) {
+                confirmService.confirm(text).then(function (confirmed) {
+                    if (confirmed) {
+                        $ctrl.currentMember.addresses.splice($index, 1);
+                        $ctrl.updateCompanyMember($ctrl.currentMember);
+                    }
+                });
+            };
+
+            $translate('customer.addresses.delete_confirm').then(showDialog, showDialog);
+        };
+
+        $ctrl.updateCompanyMember = function (companyMember) {
+            return loader.wrapLoading(function () {
+                return corporateAccountApi.updateCompanyMember(companyMember).$promise;
+            });
+        };
+
+        var components = [];
+        $ctrl.addComponent = function (component) {
+            components.push(component);
+        };
+        $ctrl.removeComponent = function (component) {
+            components = _.without(components, component);
+        };
+    }]
+});
---
+++ assets/js/account/account-profile-update.js
@@ -0,0 +1,37 @@
+﻿angular.module('storefront.account')
+.component('vcAccountProfileUpdate', {
+    templateUrl: "themes/assets/js/account/account-profile-update.tpl.liquid",
+    require: {
+        accountManager: '^vcAccountManager'
+    },
+    controller: ['storefrontApp.mainContext', '$scope', 'loadingIndicatorService', 'storefront.corporateAccountApi', function (mainContext, $scope, loader, corporateAccountApi) {
+        var $ctrl = this;
+        $ctrl.loader = loader;
+        $ctrl.currentMember = mainContext.customer;
+
+        this.$routerOnActivate = function (next) {
+            loader.wrapLoading(function () {
+                return corporateAccountApi.getCompanyMember({ id: $ctrl.currentMember.id }, function (member) {
+                    $ctrl.member = {
+                        id: member.id,
+                        firstName: member.firstName,
+                        lastName: member.lastName,
+                        email: _.first(member.emails),
+                        organizations: member.organizations,
+                        title: member.title,
+                        addresses: member.addresses
+                    };
+                }).$promise;
+            });
+        };
+
+        $ctrl.submit = function () {
+            $ctrl.member.fullName = $ctrl.member.firstName + ' ' + $ctrl.member.lastName;
+            $ctrl.member.emails = [$ctrl.member.email];
+
+            return loader.wrapLoading(function () {
+                return corporateAccountApi.updateCompanyMember($ctrl.member).$promise;
+            });
+        };
+    }]
+});
