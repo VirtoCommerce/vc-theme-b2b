95a1ffccf9feb4c6d14295a6830bd88fa21e08ea
Aleksandr Vishnyakov <av@virtoway.com>
2017-07-05 19:25:42 +0200

    Added support of token-based authentication on platform

--- assets/js/account/$account.js
+++ assets/js/account/$account.js
@@ -4,7 +4,7 @@ var moduleName = "storefront.account";
 if (storefrontAppDependencies !== undefined) {
     storefrontAppDependencies.push(moduleName);
 }
-angular.module(moduleName, ['ngResource', 'ngComponentRouter', 'credit-cards', 'pascalprecht.translate', 'ngSanitize', 'ui.indeterminate'])
+angular.module(moduleName, ['ngResource', 'ngComponentRouter', 'credit-cards', 'pascalprecht.translate', 'ngSanitize', 'satellizer', 'ui.indeterminate'])
 
 .config(['$translateProvider', function ($translateProvider) {
     $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
