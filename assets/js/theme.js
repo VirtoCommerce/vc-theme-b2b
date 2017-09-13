ed3076e50aaf086e92a719965986d2e0fe26b47b
Aleksandr Vishnyakov <asvishnyakov@hotmail.com>
2017-09-13 09:05:57 +0500

    Bulk-order and search redesigned; Refactoring

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,29 +1,5 @@
 $(function () {
-	$('.serviceLinks.dropdown, .allProducts.dropdown').on('mouseover', function () {
-		$(this).addClass('open');
-	}).on('mouseleave', function () {
-		$(this).removeClass('open');
-	});
-
-	$('.bulkOrderBody.dropdown').on('click', function () {
-		if($(this).hasClass('open')) {
-			$(this).removeClass('open');
-		}
-		else {
-			$(this).addClass('open');
-		}
-	});
-
-	$('.seoDescToggle').on('click', function () {
-		if($(this).hasClass('collapsed')) {
-			$(this).removeClass('collapsed').addClass('close');
-			$(this).text('Close');
-			$('#seoCopyDesc').show();
-		}
-		else {
-			$(this).removeClass('close').addClass('collapsed');
-			$(this).text('About this category');
-			$('#seoCopyDesc').hide();
-		}
-	});
+    $(document).on('click', '.dropdown-menu', function (e) {
+        e.stopPropagation();
+    });
 });
