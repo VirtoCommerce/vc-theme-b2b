444ee1117c93c28984712075bad7c94c8cbffd1c
Aleksandr Vishnyakov <asvishnyakov@hotmail.com>
2017-09-13 15:35:53 +0500

    Open dropdowns on mouse over

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,19 @@
 $(function () {
-    $(document).on('click', '.dropdown-menu', function (e) {
+    // do not close dropdown when element inside it was clicked
+    $(document).on('click.bs.dropdown.data-api', '.dropdown-menu', function (e) {
         e.stopPropagation();
+    });
+    var mouseOverDropdownSelector = '[data-toggle="dropdown"]:not([data-toggle-trigger="click"])';
+    // show dropdown when mouse enter to dropdown
+    $(document).on('mouseenter.bs.dropdown.data-api', mouseOverDropdownSelector + ':not([aria-expanded="true"])', $.fn.dropdown.Constructor.prototype.toggle);
+    $(document).on('mouseleave.bs.dropdown.data-api', '.dropdown', function (e) {
+        var dropdownSelector = mouseOverDropdownSelector + ':not([aria-expanded="false"])';
+        var dropdownToggle = $(e.target).parents('.dropdown').find(dropdownSelector);
+        if (dropdownToggle.length) {
+            // hide dropdown menu when mouse leave dropdown or dropdown menu
+            $.fn.dropdown.Constructor.prototype.toggle(e);
+            // unfocus dropdown: itself or as parent of element fired event
+            dropdownToggle.blur();
+        }
     });
 });
