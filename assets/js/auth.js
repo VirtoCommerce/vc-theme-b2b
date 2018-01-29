09a67544cae99dd24537ae127f94723071e6eef8
eMazeika <em@virtoway.com>
2018-01-29 13:17:59 +0200

    Revert "account api related updates"
    This reverts commit 96abc87bd53c42168f1871c9f4f49c4e4726cf50.

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,108 +1,108 @@
 ﻿angular.module('storefrontApp')
-    .factory('authService', ['storefrontApp.mainContext', '$auth', '$httpParamSerializerJQLike', '$interpolate', '$rootScope', 'storefront.accountApi',
-        function (mainContext, $auth, $httpParamSerializerJQLike, $interpolate, $rootScope, accountApi) {
-    
-    var authContext = {
-        userId: null,
-        userLogin: null,
-        fullName: null,
-        userType: null,
-        roles: null,
-        permissions: null,
-        isAuthenticated: false
-    };
+    .factory('authService', ['storefrontApp.mainContext', '$auth', '$httpParamSerializerJQLike', '$interpolate', '$rootScope', 'storefront.corporateAccountApi',
+        function (mainContext, $auth, $httpParamSerializerJQLike, $interpolate, $rootScope, corporateAccountApi) {
 
-    authContext.login = function (login, password) {
-        return $auth.login($httpParamSerializerJQLike({
-                userName: login,
-                password: password,
-                grant_type: "password"
-            }),
-            { headers: { 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' }
-        });
-    };
+            var authContext = {
+                userId: null,
+                userLogin: null,
+                fullName: null,
+                userType: null,
+                roles: null,
+                permissions: null,
+                isAuthenticated: false
+            };
 
-    authContext.fillAuthData = function () {
-        //return corporateAccountApi.getUser({ userName: mainContext.customer.userName },
-        return accountApi.get(
-            function (result) {
-                changeAuth(result)
-                $rootScope.$broadcast('loginStatusChanged', authContext);
-            },
-            function (error) { });
-    };
+            authContext.login = function (login, password) {
+                return $auth.login($httpParamSerializerJQLike({
+                    userName: login,
+                    password: password,
+                    grant_type: "password"
+                }),
+                    {
+                        headers: { 'Accept': 'application/json', 'Content-Type': 'application/x-www-form-urlencoded' }
+                    });
+            };
 
-    authContext.checkPermission = function (permission, securityScopes) {
-        //first check admin permission
-        // var hasPermission = $.inArray('admin', authContext.permissions) > -1;
-        var hasPermission = authContext.isAdministrator;
-        if (!hasPermission && permission) {
-            permission = permission.trim();
-            //first check global permissions
-            hasPermission = $.inArray(permission, authContext.permissions) > -1;
-            if (!hasPermission && securityScopes) {
-                if (typeof securityScopes === 'string' || angular.isArray(securityScopes)) {
-                    securityScopes = angular.isArray(securityScopes) ? securityScopes : securityScopes.split(',');
-                    //Check permissions in scope
-                    hasPermission = _.some(securityScopes, function (x) {
-                        var permissionWithScope = permission + ":" + x;
-                        var retVal = $.inArray(permissionWithScope, authContext.permissions) > -1;
-                        //console.log(permissionWithScope + "=" + retVal);
-                        return retVal;
+            authContext.fillAuthData = function () {
+                return corporateAccountApi.getUser({ userName: mainContext.customer.userName },
+                    function (result) {
+                        changeAuth(result)
+                        $rootScope.$broadcast('loginStatusChanged', authContext);
+                    },
+                    function (error) { });
+            };
+
+            authContext.checkPermission = function (permission, securityScopes) {
+                //first check admin permission
+                // var hasPermission = $.inArray('admin', authContext.permissions) > -1;
+                var hasPermission = authContext.isAdministrator;
+                if (!hasPermission && permission) {
+                    permission = permission.trim();
+                    //first check global permissions
+                    hasPermission = $.inArray(permission, authContext.permissions) > -1;
+                    if (!hasPermission && securityScopes) {
+                        if (typeof securityScopes === 'string' || angular.isArray(securityScopes)) {
+                            securityScopes = angular.isArray(securityScopes) ? securityScopes : securityScopes.split(',');
+                            //Check permissions in scope
+                            hasPermission = _.some(securityScopes, function (x) {
+                                var permissionWithScope = permission + ":" + x;
+                                var retVal = $.inArray(permissionWithScope, authContext.permissions) > -1;
+                                //console.log(permissionWithScope + "=" + retVal);
+                                return retVal;
+                            });
+                        }
+                    }
+                }
+                return hasPermission;
+            };
+
+            function changeAuth(results) {
+                authContext.userId = results.id;
+                authContext.roles = results.roles;
+                authContext.permissions = results.permissions;
+                authContext.userLogin = results.userName;
+                authContext.fullName = results.userLogin;
+                authContext.isAuthenticated = results.userName != null;
+                authContext.userType = results.userType;
+                authContext.isAdministrator = results.isAdministrator;
+                //Interpolate permissions to replace some template to real value
+                if (authContext.permissions) {
+                    authContext.permissions = _.map(authContext.permissions, function (x) {
+                        return $interpolate(x)(authContext);
                     });
                 }
-            }
-        }
-        return hasPermission;
-    };
+            };
 
-    function changeAuth(results) {
-        authContext.userId = results.id;
-        authContext.roles = results.roles;
-        authContext.permissions = results.permissions;
-        authContext.userLogin = results.userName;
-        authContext.fullName = results.userLogin;
-        authContext.isAuthenticated = results.userName != null;
-        authContext.userType = results.userType;
-        authContext.isAdministrator = results.isAdministrator;
-        //Interpolate permissions to replace some template to real value
-        if (authContext.permissions) {
-            authContext.permissions = _.map(authContext.permissions, function (x) {
-                return $interpolate(x)(authContext);
-            });
-        }
-    };
-
-    return authContext;
-}])
-.constant('tokenExpirationName', 'platform_access_token_expiration_time')
-.config(['$authProvider', '$provide', 'apiBaseUrl', function($authProvider, $provide, apiBaseUrl) {
-    $authProvider.loginUrl = apiBaseUrl + 'Token';
-    $authProvider.tokenName = 'access_token';
-    $authProvider.tokenPrefix = 'platform';
-    $authProvider.oauth2({
-      name: 'platform',
-      clientId: 'web'
-    });
-    $provide.decorator('SatellizerShared', ['$delegate', 'tokenExpirationName', function($delegate, tokenExpirationName) {
-        var service = $delegate;
-        var originalSetToken = service.setToken;
-        service.setToken = function(response){
-            originalSetToken.apply(service, arguments);
-            var expirationTime = Date.parse(response.data['.expires']);
-            this.SatellizerStorage.set(tokenExpirationName, expirationTime);;
+            return authContext;
+        }])
+    .constant('tokenExpirationName', 'platform_access_token_expiration_time')
+    .config(['$authProvider', '$provide', 'apiBaseUrl', function ($authProvider, $provide, apiBaseUrl) {
+        $authProvider.loginUrl = apiBaseUrl + 'Token';
+        $authProvider.tokenName = 'access_token';
+        $authProvider.tokenPrefix = 'platform';
+        $authProvider.oauth2({
+            name: 'platform',
+            clientId: 'web'
+        });
+        $provide.decorator('SatellizerShared', ['$delegate', 'tokenExpirationName', function ($delegate, tokenExpirationName) {
+            var service = $delegate;
+            var originalSetToken = service.setToken;
+            service.setToken = function (response) {
+                originalSetToken.apply(service, arguments);
+                var expirationTime = Date.parse(response.data['.expires']);
+                this.SatellizerStorage.set(tokenExpirationName, expirationTime);;
+            };
+            return service;
+        }]);
+    }])
+    .run(['$auth', 'SatellizerConfig', 'SatellizerStorage', 'tokenExpirationName', '$timeout', '$window', '$location', function ($auth, $authProvider, $authStorage, tokenExpirationName, $timeout, $window, $location) {
+        var logOut = function () {
+            $auth.logout();
+            $authStorage.remove(tokenExpirationName);
+            $window.location.href = $location.protocol() + "://" + $location.host() + ":" + $location.port() + '/account/logout';
         };
-        return service;
-    }]);
-}])
-.run(['$auth', 'SatellizerConfig', 'SatellizerStorage', 'tokenExpirationName', '$timeout', '$window', '$location', function($auth, $authProvider, $authStorage, tokenExpirationName, $timeout, $window, $location) {
-    var logOut = function(){
-        $auth.logout();
-        $authStorage.remove(tokenExpirationName);
-        $window.location.href = $location.protocol() + "://" + $location.host() + ":" + $location.port() + '/account/logout';
-    };
 
-    if ($auth.isAuthenticated()){
-        $timeout(logOut, parseFloat($authStorage.get(tokenExpirationName)) - Date.now());
-    }
-}]);
+        if ($auth.isAuthenticated()) {
+            $timeout(logOut, parseFloat($authStorage.get(tokenExpirationName)) - Date.now());
+        }
+    }]);
