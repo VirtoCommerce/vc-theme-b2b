ae6e0efb348e2a3da299ea8f38f8f6d9d5089e29
Eugeny Tatarincev <et@virtoway.com>
2018-04-06 15:53:56 +0200

    Bind USer Subscriptions and Orders to the storefront API

--- assets/js/account/account-subscriptions.js
+++ assets/js/account/account-subscriptions.js
@@ -1,4 +1,4 @@
-﻿angular.module('storefront.account')
+angular.module('storefront.account')
 .component('vcAccountSubscriptions', {
     templateUrl: "themes/assets/js/account/account-subscriptions.tpl.liquid",
     $routeConfig: [
@@ -9,20 +9,20 @@
 
 .component('vcAccountSubscriptionsList', {
     templateUrl: "account-subscriptions-list.tpl",
-    controller: ['storefront.subscriptionApi', 'confirmService', 'loadingIndicatorService', '$translate', function (subscriptionApi, confirmService, loader, $translate) {
+    controller: ['accountApi', 'confirmService', 'loadingIndicatorService', '$translate', function (accountApi, confirmService, loader, $translate) {
         var $ctrl = this;
         $ctrl.loader = loader;
         $ctrl.pageSettings = { currentPage: 1, itemsPerPageCount: 5, numPages: 10 };
         $ctrl.pageSettings.pageChanged = function () {
             loader.wrapLoading(function () {
-                return subscriptionApi.search({
+                return accountApi.searchUserSubscriptions({
                     pageNumber: $ctrl.pageSettings.currentPage,
                     pageSize: $ctrl.pageSettings.itemsPerPageCount,
                     sortInfos: $ctrl.sortInfos
-                }, function (data) {
-                    $ctrl.entries = data.results;
-                    $ctrl.pageSettings.totalItems = data.totalCount;
-                }).$promise;
+                }).then(function (response) {
+                    $ctrl.entries = response.data.results;
+                    $ctrl.pageSettings.totalItems = response.data.totalCount;
+                });
             });
         };
 
@@ -35,15 +35,15 @@
 
 .component('vcAccountSubscriptionDetail', {
     templateUrl: "account-subscription-detail.tpl",
-    controller: ['storefront.subscriptionApi', 'confirmService', 'loadingIndicatorService', '$translate', function (subscriptionApi, confirmService, loader, $translate) {
+    controller: ['accountApi', 'confirmService', 'loadingIndicatorService', '$translate', function (accountApi, confirmService, loader, $translate) {
         var $ctrl = this;
         $ctrl.loader = loader;
 
         function refresh() {
             loader.wrapLoading(function () {
-                return subscriptionApi.get({ number: $ctrl.entryNumber }, function (result) {
-                    $ctrl.subscription = angular.copy(result);
-                }).$promise;
+                return accountApi.getUserSubscription({ number: $ctrl.entryNumber }).then(function (response) {
+                    $ctrl.subscription = angular.copy(response.data);
+                });
             });
         }
 
@@ -55,19 +55,14 @@
         };
 
         $ctrl.cancel = function () {
-            //var showDialog = function (text) {
-            //    confirmService.confirm(text).then(function (confirmed) {
-            //        if (confirmed) {
+           
             loader.wrapLoading(function () {
-                return subscriptionApi.cancel({ number: $ctrl.entryNumber }, { number: $ctrl.entryNumber, cancelReason: $ctrl.cancelReason }, function (result) {
-                    $ctrl.subscription = angular.copy(result);
+                return accountApi.cancelUserSubscription({ number: $ctrl.entryNumber, cancelReason: $ctrl.cancelReason }).then(function (result) {
+                    $ctrl.subscription = angular.copy(result.data);
                     $ctrl.isCancelFormVisible = false;
-                }).$promise;
-            });
-            //        }
-            //    });
-            //};
-            //$translate('customer.subscription.cancel_confirmation').then(showDialog, showDialog);
+                    refresh();
+                });
+            });               
         };
     }]
 })
@@ -90,5 +85,4 @@
 
         return retVal;
     };
-})
-;
+});
