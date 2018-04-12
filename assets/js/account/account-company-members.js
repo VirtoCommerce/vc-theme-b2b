5acdf60cb10393cd480a2311bde7917aea4b975a
Eugeny Tatarincev <et@virtoway.com>
2018-04-12 13:31:02 +0200

    Minor refactor according to user cabinet and invitation

--- assets/js/account/account-company-members.js
+++ assets/js/account/account-company-members.js
@@ -1,228 +1,152 @@
 angular.module('storefront.account')
-.component('vcAccountCompanyMembers', {
-    templateUrl: "themes/assets/account-company-members.tpl.liquid",
-    $routeConfig: [
-     { path: '/', name: 'MemberList', component: 'vcAccountCompanyMembersList', useAsDefault: true },
-     { path: '/:member', name: 'MemberDetail', component: 'vcAccountCompanyMemberDetail' }
-    ],
-    controller: ['storefrontApp.mainContext', function (mainContext) {
-        var $ctrl = this;
-    }]
-})
+    .component('vcAccountCompanyMembers', {
+        templateUrl: "themes/assets/account-company-members.tpl.liquid",
+        $routeConfig: [
+            { path: '/', name: 'MemberList', component: 'vcAccountCompanyMembersList', useAsDefault: true },
+            { path: '/:member', name: 'MemberDetail', component: 'vcAccountCompanyMemberDetail' }
+        ],
+        controller: ['storefrontApp.mainContext', function (mainContext) {
+            var $ctrl = this;
+        }]
+    })
 
-.component('vcAccountCompanyMembersList', {
-    templateUrl: "account-company-members-list.tpl",
-    bindings: { $router: '<' },
-    controller: ['storefrontApp.mainContext', '$scope', 'accountApi', 'loadingIndicatorService', 'confirmService', '$location', '$translate', function (mainContext, $scope, accountApi, loader, confirmService, $location, $translate) {
-        var $ctrl = this;
-        $ctrl.currentMemberId = mainContext.customer.id;
-        $ctrl.newMemberComponent = null;
-        $ctrl.loader = loader;
-        $ctrl.pageSettings = { currentPage: 1, itemsPerPageCount: 5, numPages: 10 };
-        $ctrl.pageSettings.pageChanged = function () { refresh(); };
+    .component('vcAccountCompanyMembersList', {
+        templateUrl: "account-company-members-list.tpl",
+        bindings: { $router: '<' },
+        controller: ['storefrontApp.mainContext', '$scope', 'accountApi', 'loadingIndicatorService', 'confirmService', '$location', '$translate', function (mainContext, $scope, accountApi, loader, confirmService, $location, $translate) {
+            var $ctrl = this;
+            $ctrl.currentMemberId = mainContext.customer.id;
+            $ctrl.newMemberComponent = null;
+            $ctrl.loader = loader;
+            $ctrl.pageSettings = { currentPage: 1, itemsPerPageCount: 5, numPages: 10 };
+            $ctrl.pageSettings.pageChanged = function () { refresh(); };
 
-        function refresh() {
-                 loader.wrapLoading(function () {
-                     return accountApi.searchOrganizationUsers({
-                    skip: ($ctrl.pageSettings.currentPage - 1) * $ctrl.pageSettings.itemsPerPageCount,
-                    take: $ctrl.pageSettings.itemsPerPageCount,
-                    sortInfos: $ctrl.sortInfos
-                }).then(function (response) {
-                    $ctrl.entries = response.data.results;
-                    _.each($ctrl.entries, function (x) { x.role = x.roles ? x.roles[0] : undefined });
-                    $ctrl.pageSettings.totalItems = response.data.totalCount;
-                });
-            });
-        };
-        
-        $ctrl.addNewMemberFieldsConfig =[
-            {
-                field: 'CompanyName',
-                disabled: true,
-                visible: false,
-                required: false
-            },
-            {
-                field: 'Email',
-                disabled: false,
-                visible: true,
-                required: true
-            },
-            {
-                field: 'UserName',
-                disabled: false,
-                visible: true
-            },
-            {
-                field: 'Password',
-                disabled: false,
-                visible:  true
-            },
-            {
-                field: 'Roles',
-                disabled: false,
-                visible:  true,
-                required: true
-            }
-        ];
-        
-        $scope.init = function(storeId, cultureName, registrationUrl){
-            $ctrl.storeId = storeId;
-            $ctrl.cultureName = cultureName;
-            $ctrl.registrationUrl = registrationUrl;
-        };
-
-        this.$routerOnActivate = function (next) {
-            $ctrl.pageSettings.currentPage = next.params.pageNumber || $ctrl.pageSettings.currentPage;
-            refresh();
-        };
-
-        $ctrl.inviteEmailsValidationPattern = new RegExp(/((^|((?!^)([,;]|\r|\r\n|\n)))([a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*))+$/);
-        $ctrl.invite = function () {
-            $ctrl.inviteInfo.emails = $ctrl.inviteInfo.rawEmails.split(/[,;]|\r|\r\n|\n/g);
-            loader.wrapLoading(function(){
-                return accountApi.createInvitation({
-                    emails: $ctrl.inviteInfo.emails,
-                    message: $ctrl.inviteInfo.message
-                }).then(function (response) {
-                    $ctrl.cancel();
-                    $ctrl.pageSettings.pageChanged();
-                });
-            });
-        };
-
-        $ctrl.addNewMember = function () {
-            if ($ctrl.newMemberComponent.validate()) {
-                $ctrl.newMember.organizationId = mainContext.customer.organizationId;
-                $ctrl.newMember.role = $ctrl.newMember.role ? $ctrl.newMember.role.id : undefined;
-                $ctrl.newMember.storeId = $ctrl.storeId;
-
+            function refresh() {
                 loader.wrapLoading(function () {
-                    return accountApi.registerNewUser($ctrl.newMember).then(function (response) {
+                    return accountApi.searchOrganizationUsers({
+                        skip: ($ctrl.pageSettings.currentPage - 1) * $ctrl.pageSettings.itemsPerPageCount,
+                        take: $ctrl.pageSettings.itemsPerPageCount,
+                        sortInfos: $ctrl.sortInfos
+                    }).then(function (response) {
+                        $ctrl.entries = response.data.results;                       
+                        $ctrl.pageSettings.totalItems = response.data.totalCount;
+                    });
+                });
+            };
+
+            $ctrl.addNewMemberFieldsConfig = [
+                {
+                    field: 'CompanyName',
+                    disabled: true,
+                    visible: false,
+                    required: false
+                },
+                {
+                    field: 'Email',
+                    disabled: false,
+                    visible: true,
+                    required: true
+                },
+                {
+                    field: 'UserName',
+                    disabled: false,
+                    visible: true
+                },
+                {
+                    field: 'Password',
+                    disabled: false,
+                    visible: true
+                },
+                {
+                    field: 'Roles',
+                    disabled: false,
+                    visible: true,
+                    required: true
+                }
+            ];
+
+            $scope.init = function (storeId, cultureName, registrationUrl) {
+                $ctrl.storeId = storeId;
+                $ctrl.cultureName = cultureName;
+                $ctrl.registrationUrl = registrationUrl;
+            };
+
+            this.$routerOnActivate = function (next) {
+                $ctrl.pageSettings.currentPage = next.params.pageNumber || $ctrl.pageSettings.currentPage;
+                refresh();
+            };
+
+            $ctrl.inviteEmailsValidationPattern = new RegExp(/((^|((?!^)([,;]|\r|\r\n|\n)))([a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*))+$/);
+            $ctrl.invite = function () {
+                $ctrl.inviteInfo.emails = $ctrl.inviteInfo.rawEmails.split(/[,;]|\r|\r\n|\n/g);
+                loader.wrapLoading(function () {
+                    return accountApi.createInvitation({
+                        emails: $ctrl.inviteInfo.emails,
+                        message: $ctrl.inviteInfo.message
+                    }).then(function (response) {
                         $ctrl.cancel();
-                        $ctrl.pageSettings.currentPage = 1;
                         $ctrl.pageSettings.pageChanged();
                     });
                 });
-            }
-        };
+            };
 
-        $ctrl.cancel = function () {
-            $ctrl.inviteInfo = null;
-            $ctrl.newMember = null;
-        };
+            $ctrl.addNewMember = function () {
+                if ($ctrl.newMemberComponent.validate()) {
+                    $ctrl.newMember.organizationId = mainContext.customer.organizationId;
+                    $ctrl.newMember.role = $ctrl.newMember.role ? $ctrl.newMember.role.id : undefined;
+                    $ctrl.newMember.storeId = $ctrl.storeId;
 
-        $ctrl.changeStatus = function (member) {
-            loader.wrapLoading(function () {
-                var action = member.isActive ? accountApi.lockUser : accountApi.unlockUser;
-                member.isActive = !member.isActive;                
+                    loader.wrapLoading(function () {
+                        return accountApi.registerNewUser($ctrl.newMember).then(function (response) {
+                            $ctrl.cancel();
+                            $ctrl.pageSettings.currentPage = 1;
+                            $ctrl.pageSettings.pageChanged();
+                        });
+                    });
+                }
+            };
+
+            $ctrl.cancel = function () {
+                $ctrl.inviteInfo = null;
+                $ctrl.newMember = null;
+            };
+
+            $ctrl.changeStatus = function (member) {
                 loader.wrapLoading(function () {
+                    var action = member.isActive ? accountApi.lockUser : accountApi.unlockUser;
+                    member.isActive = !member.isActive;                    
                     return action(member.id);
                 });
-            });
-        };
-
-        $ctrl.edit = function (memberId) {
-            this.$router.navigate(['MemberDetail', {member: memberId, pageNumber: $ctrl.pageSettings.currentPage}]);
-        }
-
-        $ctrl.delete = function (member) {
-            var showDialog = function (text) {
-                confirmService.confirm(text).then(function (confirmed) {
-                    if (confirmed) {
-                        loader.wrapLoading(function () {
-                            return accountApi.deleteUser(member.securityAccounts[0].id).then(function (response) {
-                                $ctrl.pageSettings.pageChanged();
-                                //TODO: errors handling
-                            });
-                        });
-                    }
-                });
             };
 
-            $translate('customer.edit_company_members.delete_confirm').then(showDialog, showDialog);
-        };
-
-        $ctrl.validate = function (){
-            $ctrl.inviteForm.$setSubmitted();
-            return $ctrl.inviteForm.valid;
-        };
-
-        $ctrl.showActions = function (member) {
-            return member.id != mainContext.customer.id;
-        }
-    }]
-})
-
-.component('vcAccountCompanyMemberDetail', {
-    templateUrl: "account-company-members-detail.tpl",
-    require: {
-        accountManager: '^vcAccountManager'
-    },
-    controller: ['$q', '$rootScope', '$scope', '$window', 'accountApi', 'loadingIndicatorService', function ($q, $rootScope, $scope, $window, accountApi, loader) {
-        var $ctrl = this;
-        $ctrl.loader = loader;
-        $ctrl.fieldsConfig =[
-            {
-                field: 'CompanyName',
-                disabled: true,
-                visible: false,
-                required: false
-            },
-            {
-                field: 'Email',
-                disabled: false,
-                visible: true,
-                required: true
-            },
-            {
-                field: 'UserName',
-                disabled: true,
-                visible: false
-            },
-            {
-                field: 'Password',
-                disabled: true,
-                visible: false
-            },
-            {
-                field: 'Roles',
-                disabled: false,
-                visible:  true
+            $ctrl.edit = function (memberId) {
+                this.$router.navigate(['MemberDetail', { member: memberId, pageNumber: $ctrl.pageSettings.currentPage }]);
             }
-        ];
 
-        $ctrl.memberComponent = null;
-        
-        $scope.init = function(storeId){
-            $ctrl.storeId = storeId;
-        };
-
-        function refresh() {
-            loader.wrapLoading(function () {
-                return accountApi.getUserById($ctrl.memberNumber).then(function (response) {
-                    $ctrl.member = response.data;                   
-                });
-            });
-        }
-
-        this.$routerOnActivate = function (next) {
-            $ctrl.pageNumber = next.params.pageNumber || 1;
-            $ctrl.memberNumber = next.params.member;
-            refresh();
-        };
-
-        $ctrl.submitMember = function () {
-            if ($ctrl.memberComponent.validate()) {
-                loader.wrapLoading(function () {
-                    $ctrl.member.fullName = $ctrl.member.firstName + ' ' + $ctrl.member.lastName;
-                    $ctrl.member.emails = [$ctrl.member.email];
-                    $ctrl.member.roles = [ $ctrl.member.role.id ];
-                    return accountApi.updateUser($ctrl.member).then(function (response) {
-                        refresh();
+            $ctrl.delete = function (member) {
+                var showDialog = function (text) {
+                    confirmService.confirm(text).then(function (confirmed) {
+                        if (confirmed) {
+                            loader.wrapLoading(function () {
+                                return accountApi.deleteUser(member.id).then(function (response) {
+                                    $ctrl.pageSettings.pageChanged();
+                                    //TODO: errors handling
+                                });
+                            });
+                        }
                     });
-                });
+                };
+
+                $translate('customer.edit_company_members.delete_confirm').then(showDialog, showDialog);
             };
-        };
-    }]
-});
+
+            $ctrl.validate = function () {
+                $ctrl.inviteForm.$setSubmitted();
+                return $ctrl.inviteForm.valid;
+            };
+
+            $ctrl.showActions = function (member) {
+                return member.id != mainContext.customer.id;
+            }
+        }]
+    });
