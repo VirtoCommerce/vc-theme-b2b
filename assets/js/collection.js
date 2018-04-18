40a30ee8cde5697934f87b486d8a66d060cf6593
Andrei Kostyrin <kostyrin2@gmail.com>
2018-04-18 11:57:54 +0200

    #89 append selected branch into url

--- assets/js/collection.js
+++ assets/js/collection.js
@@ -1,22 +1,52 @@
 ﻿var storefrontApp = angular.module('storefrontApp');
 
 storefrontApp.controller('collectionController', ['$scope', '$location', function ($scope, $location) {
+    $scope.fulfillmentCenters = [];
+    $scope.currentfulfillmentCenter = null;
     var $ctrl = this;
-    $ctrl.sortModes = {
-        'manual': 'collections.sorting.featured',
-        'best-selling': 'collections.sorting.best_selling',
-        'title-ascending': 'collections.sorting.az',
-        'title-descending': 'collections.sorting.za',
-        'price-ascending': 'collections.sorting.price_ascending',
-        'price-descending': 'collections.sorting.price_descending',
-        'createddate-descending': 'collections.sorting.date_descending',
-        'createddate-ascending': 'collections.sorting.date_ascending'
-    };
-    $ctrl.viewQuery = { view: ['grid'] };
+    $ctrl.init = function() {
+        $ctrl.sortModes = {
+            'manual': 'collections.sorting.featured',
+            'best-selling': 'collections.sorting.best_selling',
+            'title-ascending': 'collections.sorting.az',
+            'title-descending': 'collections.sorting.za',
+            'price-ascending': 'collections.sorting.price_ascending',
+            'price-descending': 'collections.sorting.price_descending',
+            'createddate-descending': 'collections.sorting.date_descending',
+            'createddate-ascending': 'collections.sorting.date_ascending'
+        };
+        $ctrl.viewQuery = { view: ['grid'] };
+        $ctrl.keywordQuery = { keyword: [], branch:[] };
+        $ctrl.currentBranch = null;
+        $ctrl.searchInBranch = null;
+        
+    }
+    
     $ctrl.generatePageSizes = function (capacity, steps) {
         $ctrl.pageSizeQuery = { page_size: [capacity] };
         // for example            start: 16 stop: 16 * 3 + 1 = 49 step: 16
         $ctrl.pageSizes = _.range(capacity, capacity * steps + 1, capacity);
     }
-    $ctrl.keywordQuery = { keyword: [], branch:[] };
+    
+    
+    $ctrl.selectedBranch = function() {
+        var sc = $scope;
+        if ($ctrl.searchInBranch) {
+            $ctrl.keywordQuery.branch = null;
+        }
+        else {
+            $ctrl.keywordQuery.branch = [$scope.getCurrentFulfillment()];
+        }
+    }
+
+    $scope.getCurrentFulfillment = function() {
+        var result = _.find($scope.fulfillmentCenters, function(fulfillment){ return fulfillment.selected === true; });
+        return result.name;
+    };
+
+    $scope.$on('$locationChangeSuccess', function(event) {
+        $ctrl.searchInBranch = angular.isDefined($ctrl.keywordQuery.branch[0]);
+    });
+
+    $ctrl.init();
 }]);
--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -1,34 +1,28 @@
 ﻿var storefrontApp = angular.module('storefrontApp');
 
-storefrontApp.controller('inventoryController', ['$scope', 'dialogService', 'fulfillmentCenterService', '$location', '$window', function ($scope, dialogService, fulfillmentCenterService, $location, $window) {
+storefrontApp.controller('inventoryController', ['$scope', 'dialogService', 'fulfillmentCenterService', function ($scope, dialogService, fulfillmentCenterService) {
     $inventory = this;
-    $inventory.inStock = false;
-    $scope.customerId = "ss";
-    $inventory.selectedFulfillmentCenter = null;
-    
+    $inventory.parentfulfillmentCenters = $scope.fulfillmentCenters;
+    $inventory.currentfulfillmentCenter = $scope.currentfulfillmentCenter;
+
     $scope.searchFulfillmentCenters = function() {
-        fulfillmentCenterService.searchFulfillmentCenters({ searchPhrase: $scope.searchPhrase }).then(function(response) {
-            $scope.fulfillmentCenters = response.data.results;
-        });
+        // fulfillmentCenterService.searchFulfillmentCenters({ searchPhrase: $scope.searchPhrase }).then(function(response) {
+        //     $scope.fulfillmentCenters = response.data.results;
+        // });
+        //$scope.fulfillmentCenters = $inventory.parentfulfillmentCenters;
+        $inventory.parentfulfillmentCenters.push({name: "branch1", selected: false},{ name: "branch2", selected: false});
+        
     };
 
-    $inventory.selectFulfillmentCenter = function() {
-        dialogService.showDialog({ isFilter: true }, 'universalDialogController', 'storefront.select-fulfillment-center-dialog.tpl');
+    $scope.selectFulfillmentCenter = function() {
+        //dialogService.showDialog({ isFilter: true }, 'universalDialogController', 'storefront.select-fulfillment-center-dialog.tpl');
+        $scope.searchFulfillmentCenters();
+        $inventory.currentfulfillmentCenter = $inventory.parentfulfillmentCenters[0];
+        $inventory.currentfulfillmentCenter.selected = true;
+        //$scope.fulfillmentCenters = angular.copy($inventory.parentfulfillmentCenters); 
     };
 
     $scope.fulfillmentCenterToAddress = function (fulfillmentCenter) {
         return fulfillmentCenterService.toAddress(fulfillmentCenter);
     };
-
-    $inventory.queryInStock = function() {
-        var sc = $scope;
-        var inv = $inventory;
-        // $location.search('branch', $inventory.query);
-        // $location.search('id','123');
-        // $window.location.reload();
-    }
-
-    $scope.$watch('inStock', function (newVal) {
-        var sc = $scope;
-    });
 }]);
