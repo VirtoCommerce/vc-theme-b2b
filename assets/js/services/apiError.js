d471048f97f231aad53158592c4a319ed402fcea
Nikolai Prohorov <prohorov.nn@gmail.com>
2018-04-19 16:36:22 +0200

    Fixed ';'

--- assets/js/services/apiError.js
+++ assets/js/services/apiError.js
@@ -1,16 +1,15 @@
 var storefrontApp = angular.module('storefrontApp');
 
-storefrontApp.service('apiErrorService', ['$rootScope', function ($rootScope) {
+storefrontApp.service('apiErrorService', ['$rootScope', function($rootScope) {
     return {
-        clearErrors: function ($scope) {
+        clearErrors: function($scope) {
             $scope.errorMessage = null;
             $scope.errors = null;
         },
-        handleErrors: function ($scope, response) {
-            if (!response.data.succeeded) 
+        handleErrors: function($scope, response) {
+            if (!response.data.succeeded)
                 $scope.errorMessage = response.data.message;
-                $scope.errors = response.data.errors;
-            }
+            $scope.errors = response.data.errors;
         }
     }
 }]);
