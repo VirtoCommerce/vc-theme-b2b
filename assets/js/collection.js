16e0344d147a39ba6905c21b1b0001e84154ee49
Andrey Kostyrin <kostyrin2@gmail.com>
2018-04-20 10:07:30 +0200

    fulfillment template (#101)
    * #95 Fulfillment center details
    
    * #95 view
    
    * #95 template main
    
    * #95 templates/fulfillment-center.liquid
    
    * #95 fulfillment-center templ
    
    * #95 fulfillment div
    
    * #95 fulfillment template
    
    * #89 widget
    
    * #89 append selected branch into url
    
    * #89 widget branch
    
    * #88 popup fulfillment
    
    * #95 fulfillment details url

--- assets/js/collection.js
+++ assets/js/collection.js
@@ -1,22 +1,69 @@
 ﻿var storefrontApp = angular.module('storefrontApp');
 
-storefrontApp.controller('collectionController', ['$scope', '$location', function ($scope, $location) {
+storefrontApp.controller('collectionController', ['$scope', '$location', '$localStorage', function ($scope, $location, $localStorage) {
+    $scope.currentFulfillmentCenter = { id: null };
     var $ctrl = this;
-    $ctrl.sortModes = {
-        'manual': 'collections.sorting.featured',
-        'best-selling': 'collections.sorting.best_selling',
-        'title-ascending': 'collections.sorting.az',
-        'title-descending': 'collections.sorting.za',
-        'price-ascending': 'collections.sorting.price_ascending',
-        'price-descending': 'collections.sorting.price_descending',
-        'createddate-descending': 'collections.sorting.date_descending',
-        'createddate-ascending': 'collections.sorting.date_ascending'
-    };
-    $ctrl.viewQuery = { view: ['grid'] };
+    $ctrl.init = function() {
+        $ctrl.sortModes = {
+            'manual': 'collections.sorting.featured',
+            'best-selling': 'collections.sorting.best_selling',
+            'title-ascending': 'collections.sorting.az',
+            'title-descending': 'collections.sorting.za',
+            'price-ascending': 'collections.sorting.price_ascending',
+            'price-descending': 'collections.sorting.price_descending',
+            'createddate-descending': 'collections.sorting.date_descending',
+            'createddate-ascending': 'collections.sorting.date_ascending'
+        };
+        $ctrl.viewQuery = { view: ['grid'] };
+        $ctrl.keywordQuery = { keyword: [], branch:[] };
+        if ($localStorage['selectedBranch']) {
+            $scope.currentFulfillmentCenter = $localStorage['selectedBranch'];
+        }
+    }
+    
     $ctrl.generatePageSizes = function (capacity, steps) {
         $ctrl.pageSizeQuery = { page_size: [capacity] };
         // for example            start: 16 stop: 16 * 3 + 1 = 49 step: 16
         $ctrl.pageSizes = _.range(capacity, capacity * steps + 1, capacity);
     }
-    $ctrl.keywordQuery = { keyword: [] };
+    
+    
+    $ctrl.selectedBranch = function() {
+        var sc = $scope;
+        if ($ctrl.searchInBranch) {
+            $ctrl.keywordQuery.branch = null;
+            $ctrl.searchInBranch = false;
+        }
+        else {
+            $ctrl.keywordQuery.branch = [$scope.getCurrentFulfillment()];
+            $ctrl.searchInBranch = true;
+        }
+    }
+
+    $scope.getCurrentFulfillment = function() {
+        var result = null;
+        if ($scope.currentFulfillmentCenter) {
+            result = $scope.currentFulfillmentCenter.id;
+        }
+        return result;
+    };
+ 
+    $ctrl.checkedBranch = function() {
+        $ctrl.searchInBranch = false;
+        if ($ctrl.keywordQuery.branch && $scope.currentFulfillmentCenter) {
+            $ctrl.searchInBranch = $ctrl.keywordQuery.branch[0] === $scope.currentFulfillmentCenter.id;
+        }
+    }
+
+    $scope.$on('$locationChangeSuccess', function(event) {
+        $ctrl.checkedBranch();
+    });
+
+    $scope.$watch('currentFulfillmentCenter.id', function (fulfillmentCenterId) {
+        if ($ctrl.searchInBranch && fulfillmentCenterId) {
+            $ctrl.searchInBranch = false;
+        }
+    }, true);
+
+    $ctrl.init();
 }]);
--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -1,17 +1,37 @@
 ﻿var storefrontApp = angular.module('storefrontApp');
 
-storefrontApp.controller('inventoryController', ['$scope', 'dialogService', 'fulfillmentCenterService', function ($scope, dialogService, fulfillmentCenterService) {
+storefrontApp.controller('inventoryController', ['$scope', 'dialogService', 'fulfillmentCenterService', '$localStorage', function ($scope, dialogService, fulfillmentCenterService, $localStorage) {
+    $inventory = this;
+    $scope.fulfillmentCenters = [];
+    $scope.widgetBranchUrl = null;
+
     $scope.searchFulfillmentCenters = function() {
-        fulfillmentCenterService.searchFulfillmentCenters({ searchPhrase: $scope.searchPhrase }).then(function(response) {
-            $scope.fulfillmentCenters = response.data.results;
-        });
+        // fulfillmentCenterService.searchFulfillmentCenters({ searchPhrase: $scope.searchPhrase }).then(function(response) {
+        //     $scope.fulfillmentCenters = response.data.results;
+        // });
+        $scope.fulfillmentCenters = [{name: "branch1", id: "1", city:"NewYork", postalCode: "77777"},{ name: "branch2", id: "2", city:"Chicago", postalCode: "88888"}];
     };
 
     $scope.selectFulfillmentCenter = function() {
-        dialogService.showDialog({ isFilter: true }, 'universalDialogController', 'storefront.select-fulfillment-center-dialog.tpl');
+        var dialogInstance = dialogService.showDialog({ isFilter: true }, 'universalDialogController', 'storefront.select-fulfillment-center-dialog.tpl');
+        dialogInstance.result.then(function(result) {
+            _.extend($scope.currentFulfillmentCenter, result);
+            $localStorage['selectedBranch'] = $scope.currentFulfillmentCenter;
+            $scope.switchTemplate();
+        });
     };
 
     $scope.fulfillmentCenterToAddress = function (fulfillmentCenter) {
         return fulfillmentCenterService.toAddress(fulfillmentCenter);
     };
+
+    $scope.switchTemplate = function () {
+        if ($scope.currentFulfillmentCenter && $scope.currentFulfillmentCenter.id) {
+            $scope.widgetBranchUrl = "selected-branch";
+        } else {
+            $scope.widgetBranchUrl = "unselected-branch";
+        }
+    }
+
+    $scope.switchTemplate();
 }]);
