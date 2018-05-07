ff587fa745042fda5936bc6d1d5c21ddf3b33fd1
Andrei Kostyrin <kostyrin2@gmail.com>
2018-05-07 22:11:42 +0200

    #79 flying-placeholder add label

--- assets/js/common-components/flying-placeholder.js
+++ assets/js/common-components/flying-placeholder.js
@@ -1,22 +1,33 @@
 angular.module('storefrontApp')
-.component('vcFlyingPlaceholder', {
-    templateUrl: "themes/assets/flying-placeholder.tpl.html",
-    bindings: {
-        value: '=',
-        form: '=',
-        name: '@',
-        inputClass: '<',
-        placeholder: '@'
-    },
-    controller: [function () {
+.directive('vcFlyingPlaceholder', function() {
+    return {
+        controller: ['$scope', function vcFlyingPlaceholderController($scope) {
+            //console.log($scope);
+            
+          }],
+        scope: {
+            elementName: "@name",
+            elementModel: "@ngModel" 
+        },
+        templateUrl: '',
+        link: function(scope, e, attr) {
+            //console.log(scope);
+            //console.log(e);
+            e.before('<label class="vc-flying-placeholder-class" ng-class="{\'keyuped\': ' + scope.elementModel + '.length}" for="' + scope.elementName + '"><span>' + attr.vcFlyingPlaceholder + '</span></label>');
+        }
+    };
+  });
+
+
+  var storefrontApp = angular.module('storefrontApp');
+
+  storefrontApp.controller('flyingController', ['$q', '$scope', 
+      function ($q, $scope) {
         var $ctrl = this;
-    }]
-});
-// .directive('vcFlyingPlaceholder', function() {
-//     return {
-//         controller: ['$scope', function vcFlyingInputController($scope) {
-//             console.log($scope);
-//           }],
-//         templateUrl: 'themes/assets/flying-placeholder.tpl.html'
-//     };
-//   });
+       // console.log($scope);
+        
+        $scope.$watch('username', function (address) {
+            //console.log($scope);
+        }, true);
+
+      }]);
