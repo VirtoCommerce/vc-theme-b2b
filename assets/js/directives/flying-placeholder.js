0cb1646bef8ff451867ec42d31945a1f92333ff7
Andrei Kostyrin <kostyrin2@gmail.com>
2018-05-08 19:52:04 +0200

    #79 flying-placeholder-class and directive

--- assets/js/directives/flying-placeholder.js
+++ assets/js/directives/flying-placeholder.js
@@ -4,16 +4,29 @@ angular.module('storefrontApp')
         controller: ['$scope', function vcFlyingPlaceholderController($scope) {
             //console.log($scope);
             
-          }],
+        }],
         scope: {
             elementName: "@name",
             elementModel: "@ngModel" 
         },
+        require: 'ngModel',
         templateUrl: '',
-        link: function(scope, e, attr) {
+        link: function(scope, element, attr, ngModel) {
             //console.log(scope);
-            //console.log(e);
-            e.before('<label class="vc-flying-placeholder-class" ng-class="{\'keyuped\': ' + scope.elementModel + '.length}" for="' + scope.elementName + '"><span>' + attr.vcFlyingPlaceholder + '</span></label>');
+            //console.log(element);
+            var labelContent = '<label class="vc-flying-placeholder-class" for="' + scope.elementName + '">' + attr.vcFlyingPlaceholder + '</label>';
+            element.after(labelContent);
+            var parentElement = element.parent();
+            parentElement.addClass("vc-flying-placeholder-group-class");
+            
+            scope.$watch(function() {
+                if (ngModel.$modelValue) {
+                    parentElement.addClass("keyuped");
+                }
+                else {
+                    parentElement.removeClass("keyuped");
+                }
+            });
         }
     };
   });
