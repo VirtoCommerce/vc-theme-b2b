d151c913a4c60011d280f784645d263e1af666de
Andrei Kostyrin <kostyrin2@gmail.com>
2018-05-10 13:16:23 +0200

    #79 register.liquid

--- assets/js/directives/flying-placeholder.js
+++ assets/js/directives/flying-placeholder.js
@@ -2,19 +2,11 @@ angular.module('storefrontApp')
 .directive('vcFlyingPlaceholder', function() {
     return {
         controller: ['$scope', function vcFlyingPlaceholderController($scope) {
-            //console.log($scope);
-            
         }],
-        scope: {
-            elementName: "@name",
-            elementModel: "@ngModel" 
-        },
         require: 'ngModel',
         templateUrl: '',
         link: function(scope, element, attr, ngModel) {
-            //console.log(scope);
-            //console.log(element);
-            var labelContent = '<label class="vc-flying-placeholder-class" for="' + scope.elementName + '">' + attr.vcFlyingPlaceholder + '</label>';
+            var labelContent = '<label class="vc-flying-placeholder-class" for="' + attr.name + '">' + attr.vcFlyingPlaceholder + '</label>';
             element.after(labelContent);
             var parentElement = element.parent();
             parentElement.addClass("vc-flying-placeholder-group-class");
