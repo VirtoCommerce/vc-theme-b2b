bceaa4b11cc45de17e87859b8acfeeda8156c34b
ilyawzrd <ilya.ivanov6@gmail.com>
2019-10-23 18:35:03 +0200

    fix wrong value for sorting options,comment best-selling option since it's not working correctly for now

--- assets/js/collection.js
+++ assets/js/collection.js
@@ -5,25 +5,25 @@ storefrontApp.controller('collectionController', ['$scope', '$location',  functi
     var $ctrl = this;
     $ctrl.init = function() {
         $ctrl.sortModes = {
-            'manual': 'collections.sorting.featured',
-            'best-selling': 'collections.sorting.best_selling',
-            'title-ascending': 'collections.sorting.az',
-            'title-descending': 'collections.sorting.za',
+            'priority-descending;name-ascending': 'collections.sorting.featured',
+            // 'best-selling': 'collections.sorting.best_selling',
+            'name-ascending': 'collections.sorting.az',
+            'name-descending': 'collections.sorting.za',
             'price-ascending': 'collections.sorting.price_ascending',
             'price-descending': 'collections.sorting.price_descending',
             'createddate-descending': 'collections.sorting.date_descending',
             'createddate-ascending': 'collections.sorting.date_ascending'
         };
         $ctrl.viewQuery = { view: ['grid'] };
-        $ctrl.keywordQuery = { keyword: [] };       
+        $ctrl.keywordQuery = { keyword: [] };
     }
-    
+
     $ctrl.generatePageSizes = function (capacity, steps) {
         $ctrl.pageSizeQuery = { page_size: [capacity] };
         // for example            start: 16 stop: 16 * 3 + 1 = 49 step: 16
         $ctrl.pageSizes = _.range(capacity, capacity * steps + 1, capacity);
     }
 
-  
+
     $ctrl.init();
 }]);
