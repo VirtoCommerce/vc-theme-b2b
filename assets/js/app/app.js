9d3c4e0689ac810c4669cda8fe33a30714d4c3a1
Anton <superiorboroda@gmail.com>
2020-09-04 10:27:06 +0300

    VP-4041: Load Customer Info before angularJs app started. (#204)
    * VP-4041: Load Customer Info before angularJs app started.
    
    Draft
    
    * VP-4041: Make up draft code
    
    * VP-4041: Make up draft code
    
    Co-authored-by: Aleksandr Vishniakov <av@virtoway.com>

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -7,7 +7,8 @@
     'vcRecaptcha',
     'storefrontApp.consts',
     'mgo-angular-wizard',
-    'angularjs-dropdown-multiselect'
+    'angularjs-dropdown-multiselect',
+    'storefrontApp.customerInfo'
 ];
 var storefrontApp = angular.module('storefrontApp', storefrontAppDependencies);
 
